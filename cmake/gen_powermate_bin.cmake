05219345ddcd4f441ac49adbd150fb9a633d66ca
YoungSoo Shin <shinys000114@gmail.com>
2026-02-05 18:41:59 +0900

    refactor: extract single-bin generation logic to gen_powermate_bin.cmake and update CMakeLists.txt
    use command idf.py powermate_gen_single_bin
    
    Signed-off-by: YoungSoo Shin <shinys000114@gmail.com>
    Change-Id: I98623995f071ee16f19b018de337631980e606ec

---
+++ cmake/gen_powermate_bin.cmake
@@ -0,0 +1,24 @@
+if (NOT TARGET powermate_gen_single_bin)
+    add_custom_target(
+            powermate_gen_single_bin
+            COMMAND ${CMAKE_COMMAND} -E echo "Merge bin files to ${PROJECT_VER}.bin"
+            COMMAND ${ESPTOOLPY} --chip ${IDF_TARGET} merge_bin -o ${PROJECT_VER}.bin @flash_args
+            COMMAND ${CMAKE_COMMAND} -E echo "Merge bin done"
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+            DEPENDS gen_project_binary bootloader
+            VERBATIM USES_TERMINAL
+    )
+endif()
+
+# Flash bin ${PROJECT_VER}.bin to target chip
+if (NOT TARGET powermate_flash_single_bin)
+    add_custom_target(
+            powermate_flash_single_bin
+            COMMAND ${CMAKE_COMMAND} -E echo "Flash merged bin ${PROJECT_VER}.bin to address 0x0"
+            COMMAND ${ESPTOOLPY} --chip ${IDF_TARGET} write_flash 0x0 ${PROJECT_VER}.bin
+            COMMAND ${CMAKE_COMMAND} -E echo "Flash merged bin done"
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+            DEPENDS powermate_gen_single_bin
+            VERBATIM USES_TERMINAL
+    )
+endif()
